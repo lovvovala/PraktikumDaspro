746c9e9a54af0b6a036f7aed7a2c185ab7196b61
lovvovala <rachmahnurchotimah@gmail.com>
2025-10-01 08:08:17 +0700

    Percobaan 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -11,6 +11,8 @@ public class ifCetakKRS20 {
         if (uktLunas){
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak, Silahkan lunasi UKT terlebih dahulu");
         }
     
     }
