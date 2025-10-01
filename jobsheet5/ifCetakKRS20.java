cbbba33b7e2a4aedbc6e73e1899a0560288d4d01
lovvovala <rachmahnurchotimah@gmail.com>
2025-10-01 09:53:18 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -8,12 +8,11 @@ public class ifCetakKRS20 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas){
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak, Silahkan lunasi UKT terlebih dahulu!");
-        }
+        String hasil = uktLunas 
+                        ? "Pembayaran UKT terverifikasi\nSilahkan Cetak KRS dan minta tanda tangan DPA"
+                        : "Registrasi ditolak, Silahkan lunasi UKT terlebih dahulu!";
+
+        System.out.println(hasil);
     
     }
 }
