d9d0db9387907e7c5225d4a4675e6454b449a0ec
lovvovala <rachmahnurchotimah@gmail.com>
2025-10-01 10:23:32 +0700

    Tugas 2

---
+++ jobsheet5/tugas2j5.java
@@ -0,0 +1,21 @@
+package PraktikumDaspro.jobsheet5;
+import java.util.Scanner;
+public class tugas2j5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int angka;
+
+        System.out.print("Masukkan angka: ");
+        angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        input.close();
+    }
+}
+
